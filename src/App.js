6faa3ab3b025bc1e0384093cbe5d42d2bc2b9ca4
Shushan04 <ash118817@gmail.com>
2023-07-24 17:46:15 +0400

    set up folders

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Weather from './component/WeatherApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Weather/>
+    </div>
+  );
+}
+
+export default App;
